58404367689e9455fed29db9a01a4e0b10e9600f
Mohamed Hesham <mohamedhesham011010@gmail.com>
2026-07-01 03:44:03 +0300

    fix: add optional chaining in useEffect dependancy hooks of HomeView

--- src/components/Views/HomeView.js
+++ src/components/Views/HomeView.js
@@ -127,7 +127,7 @@ Address the user directly by their personal name (${pName}) and refer to their b
 
   useEffect(() => {
     generateDailyBrief();
-  }, [GC.crm.leads.length, GC.tasks.items.length, GC.finance.entries.length, lang]);
+  }, [GC.crm?.leads?.length, GC.tasks?.items?.length, GC.finance?.entries?.length, lang]);
 
   // Compute Health Score
   let healthScore = 40;
