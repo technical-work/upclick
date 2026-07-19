b0b4d5be865d5d5a15399339f16481290e82475f
Ahmed Sherif <sherifsh126@gmail.com>
2026-07-19 16:35:37 +0300

    uploud

--- src/components/Views/BillingView.js
+++ src/components/Views/BillingView.js
@@ -15,6 +15,7 @@ export default function BillingView() {
   const isRTL = lang === 'ar';
 
   
+  
   // Dynamic values from tenantConfig or defaults
   const planStarterName = tenantConfig?.planStarterName || 'Starter';
   const planStarterPrice = tenantConfig?.planStarterPrice !== undefined ? Number(tenantConfig.planStarterPrice) : 499;
